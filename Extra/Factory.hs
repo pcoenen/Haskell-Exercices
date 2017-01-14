acba565feec32c9e3075ece713e1bd4181fee639
Pieter-Jan <pieterjan.coenen@gmail.com>
2017-01-14 12:30:07 +0100

    better version

--- Extra/Factory.hs
+++ Extra/Factory.hs
@@ -20,8 +20,8 @@ data Car = ExpensiveCar | CheapCar | Bike
     deriving (Ord , Show , Eq)
 
 --Exercise 1
-data Station a b = M [(a, Int)] b | L [Station a b]
-    deriving (Ord , Show , Eq)
+data Station a b = M [(a,Int)] b | L [Station a b]
+    deriving (Eq, Ord, Show)
 
 machine :: [(a, Int)] -> b -> Station a b
 machine = M
@@ -31,50 +31,69 @@ combine = L
 
 -- Exercise 2
 trivial :: (Bounded a,Enum a) => Station a a
-trivial = undefined
--- trivial = L [M [((toEnum x),1)] (toEnum x) | x <- [fromEnum (minBound :: a).. fromEnum (maxBound :: a)]] 
+trivial = combine $ map (\x -> machine [(x,1)] x) $ enumFromTo minBound maxBound
 
 --Exercise 3
-data Resources a = R [a]
-    deriving (Ord , Show , Eq)
+data Resources a = R [(a,Int)]
 
 startResources :: Resources a
 startResources = R []
 
 amount :: Ord a => Resources a -> a -> Int
-amount (R l) a = sum $ map (\x -> if x==a then 1 else 0) l
+amount (R list) r = case lookup r list of
+                        Just v -> v 
+                        Nothing -> 0
 
 insert :: Ord a => Resources a -> a -> Resources a
-insert (R l) a = R (a:l)
+insert (R []) r = R [(r,1)]
+insert (R ((e,a):ls)) r
+    | e == r = R ((e,a+1):ls)
+    | otherwise = do
+                    let R nl = insert (R ls) r
+                    R ((e,a):nl)
 
-insertTimes (R l) a 0 = R l
-insertTimes (R l) a n = insertTimes (R (a:l)) a (n-1)
+delete :: Ord a => Resources a -> a -> Int -> Resources a
+delete (R ((k,v):ls)) r a
+    | k == r = if v-a == 0
+                then R ls
+                else R ((k,v-a):ls)
+    | otherwise = do
+                    let R nl = delete (R ls) r a
+                    R ((k,v):nl)
+
+deleteAll :: Ord a => [(a,Int)] -> Resources a -> Resources a
+deleteAll [] rlist = rlist
+deleteAll ((r,a):xs) rlist = deleteAll xs (delete rlist r a)
 
 --Exercise 4
 run :: Ord a => [(a,Int)] -> b -> Resources a -> [a] -> ([a],[b])
-run n b al [] 
-    | filled n al   = ([],[b])
-    | otherwise     = ([],[])
-run n b al (x:xs)
-    | filled n al               = (\(a,y) -> (a,b:y)) (run n b startResources (x:xs))
-    | needed n x > amount al x  = run n b (insert al x) xs
-    | otherwise                 = (\(a,y) -> (x:a,y)) (run n b al xs)
+run neededR product currentR [] 
+    |enough neededR currentR    = (\(a,b) -> (a,product:b)) $ run neededR product (deleteAll neededR currentR) []
+    |otherwise = ([],[])
+run neededR product currentR (r:rs)
+    |enough neededR currentR    = (\(a,b) -> (a,product:b)) $ run neededR product (deleteAll neededR currentR) (r:rs)
+    |needed neededR currentR r  = run neededR product (insert currentR r) rs
+    |otherwise                  = (\(a,b) -> (r:a,b)) $ run neededR product currentR rs
 
+enough :: Ord a => [(a,Int)] -> Resources a -> Bool 
+enough [] _ = True
+enough ((r,a):ls) currentR
+    |amount currentR r == a = enough ls currentR
+    |otherwise = False 
 
-
-needed [] a = 0
-needed ((x,n):l) a
-    | x == a    = n
-    | otherwise = needed l a
-
-filled :: Ord a => [(a,Int)] -> Resources a -> Bool
-filled [] _ = True
-filled ((a,n):l) al
-    | amount al a == n  = filled l al
-    | otherwise         = False
+needed :: Ord a => [(a,Int)] -> Resources a -> a -> Bool 
+needed neededR currentR r = case lookup r neededR of
+                                Just x -> if x > amount currentR r
+                                            then True
+                                            else False
+                                Nothing -> False
+                        
 
 --Exercise 5
 runStation :: Ord a => Station a b -> [a] -> ([a],[b])
-runStation (M x y) i    = run x y startResources i
-runStation (L []) i     = (i,[])
-runStation (L (x:xs)) i = (\ (o,(a,b)) -> (a,o++b)) $ (\ (a,b) -> (b, (runStation (L xs) a))) (runStation x i)
+runStation (M neededR product) inputR = run neededR product startResources inputR
+runStation (L []) inputR = (inputR,[])
+runStation (L (m:ms)) inputR = do
+                                let (a,b) = runStation m inputR
+                                let (an,bn) = runStation (L ms) a
+                                (an,b ++ bn)
